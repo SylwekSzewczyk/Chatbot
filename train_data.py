b291087f675514f6642a2c6b8db3aabdd91be57a
SylwekSzewczyk <sylwekszewczyk@interia.pl>
2020-01-23 22:06:42 +0100

    keras model applied

--- train_data.py
+++ train_data.py
@@ -13,6 +13,11 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
 
+
+'''
+Data preparation segment
+
+'''
 data_file = open('intents.json').read()
 intents = json.loads(data_file)
 words = []
@@ -58,3 +63,22 @@ random.shuffle(training)
 training = np.array(training)
 x_train = list(training[:,0])
 y_train = list(training[:,1])
+
+'''
+
+Deep learning model application
+
+'''
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(x_train[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(y_train[0]), activation = 'softmax'))
+
+sgd = SGD(lr=0.01, momentum = 1e-6, decay = 0.9, nesterov = True)
+model.compile(loss = 'categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
+
+model_fit = model.fit(np.array(x_train), np.array(y_train), epochs = 200, batch_size = 5, verbose = 1)
+model.save('model.h5', model_fit)
