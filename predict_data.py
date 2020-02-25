b5220a71e222c97c0ba9d77922862f71e74b69a0
SylwekSzewczyk <sylwekszewczyk@interia.pl>
2020-02-25 20:22:03 +0100

    update

--- predict_data.py
+++ predict_data.py
@@ -24,3 +24,25 @@ class Chatbot:
         sentence_words = nltk.word_tokenize(sentence)
         sentence_words = [lemmatizer.lemmatize(w.lower()) for w in sentence_words]
         return sentence_words
+    
+    def bag_of_words(self, sentence, words):
+        
+        sentence_words = self.clean_up_sentence(sentence)
+        bag = [0]*len(words)
+        for s in sentence_words:
+            for i, w in enumerate(words):
+                if w == s:
+                    bag[i] = 1
+        return np.array(bag)
+    
+    def predict_class(self, sentence, model):
+        
+        bag = self.bag_of_words(sentence, words)
+        res = model.predict(np.array([bag]))[0]
+        THRESHOLD = 0.25
+        results = [[i,r] for i, r in enumerate(res) if r>THRESHOLD]
+        results.sort(key = lambda x: x[1], reverse = True)
+        return_list = []
+        for r in results:
+            return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+        return return_list
