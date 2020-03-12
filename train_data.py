de06955176ee3a47d9684b81f88da94b713983fe
SylwekSzewczyk <sylwekszewczyk@interia.pl>
2020-03-12 10:46:23 +0100

    working project, add desktop app

--- train_data.py
+++ train_data.py
@@ -73,12 +73,12 @@ Deep learning model application
 model = Sequential()
 model.add(Dense(128, input_shape=(len(x_train[0]),), activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(64, activation = 'relu'))
+model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(len(y_train[0]), activation = 'softmax'))
+model.add(Dense(len(y_train[0]), activation='softmax'))
 
-sgd = SGD(lr=0.01, momentum = 1e-6, decay = 0.9, nesterov = True)
-model.compile(loss = 'categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-model_fit = model.fit(np.array(x_train), np.array(y_train), epochs = 200, batch_size = 5, verbose = 1)
-model.save('model.h5', model_fit)
+model_fit = model.fit(np.array(x_train), np.array(y_train), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', model_fit)
